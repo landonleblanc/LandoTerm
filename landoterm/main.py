f6be3b077f98b33b11e8ad23bcf86311d83d8c3f
Landon LeBlanc <landonleblanc@gmail.com>
2024-09-13 23:07:48 -0700

    added terminal input and ouput . Added button and drop down disable states

--- landoterm/main.py
+++ landoterm/main.py
@@ -1,9 +1,7 @@
 import sys
 import logging
 import serial
-from PySide6.QtGui import QIcon
-from PySide6.QtCore import QSize
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QLineEdit, QLabel, QComboBox, QHBoxLayout
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QTextEdit, QLineEdit, QLabel, QComboBox, QHBoxLayout
 
 logging.basicConfig(filename='landoterm.log', level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -56,14 +54,13 @@ class MainWindow(QMainWindow):
         self.port_dropdown.addItems(self.ports)
         self.baudrate_dropdown.addItems(map(str, [1200, 2400, 9600, 19200, 38400, 57600, 115200]))
         self.connect_button = QPushButton("Connect", self)
-        self.output_box = QLabel(self)
-
+        self.terminal_output = QTextEdit(self)
+        self.terminal_output.setReadOnly(True)
+        self.input_line = QLineEdit(self)
+        self.input_line.setDisabled(True)
 
         # Set layout
         main_layout = QVBoxLayout()
-
-        # Create a horizontal layout to combine port, baudrate, and connect button
-      
         port_layout = QHBoxLayout()
         port_layout.addWidget(self.port_label)
         port_layout.addWidget(self.port_dropdown)
@@ -73,46 +70,78 @@ class MainWindow(QMainWindow):
         baud_layout = QHBoxLayout()
         baud_layout.addWidget(self.baudrate_label)
         baud_layout.addWidget(self.baudrate_dropdown)
-        self.baudrate_label.setFixedWidth(65)  # No stretch for baudrate_label
-        baud_layout.setStretch(1, 3)  # Stretch for baudrate_dropdown
+        self.baudrate_dropdown.setCurrentIndex(2)
+        self.baudrate_label.setFixedWidth(65)
+        baud_layout.setStretch(1, 3)
         bar_layout = QHBoxLayout()
         bar_layout.addLayout(port_layout)
         bar_layout.addLayout(baud_layout)
         bar_layout.addWidget(self.connect_button)
-
-        bar_layout.setStretch(0, 4)  # No stretch for port_label
-        bar_layout.setStretch(1, 3)  # Stretch for port_dropdown
-        bar_layout.setStretch(2, 3)  # No stretch for baudrate_label
+        bar_layout.setStretch(0, 4)
+        bar_layout.setStretch(1, 3)
+        bar_layout.setStretch(2, 3)
 
         # Add the combined horizontal layout and output box to the main layout
         main_layout.addLayout(bar_layout)
-        main_layout.addWidget(self.output_box)
+        main_layout.addWidget(self.terminal_output)
+        main_layout.addWidget(self.input_line)
 
         container = QWidget()
         container.setLayout(main_layout)
         self.setCentralWidget(container)
 
         self.connect_button.clicked.connect(self.connect)
+        self.refresh_button.clicked.connect(self.update_ports)
+        self.input_line.returnPressed.connect(self.send_input)
 
+    def send_input(self):
+        user_input = self.input_line.text()
+        if user_input:
+            self.terminal_output.append(f"> {user_input}")
+            self.s.send(user_input.encode())  # Send the input via self.s.send
+            self.input_line.clear()  # Clear the input line after sending
 
-    def update_port_list(self):
-        try:
-            ports = self.settings.get_ports()
-            return ports
-        except Exception as e:
-            logging.error(e)
-            return []
+    def disconnect(self):
+        logging.info("Disconnecting")
+        self.s.close()
+        self.terminal_output.append(f'Disconnected from {self.s.port}')
+        self.connect_button.setText("Connect")
+        self.connect_button.clicked.disconnect()
+        self.connect_button.clicked.connect(self.connect)
+        self.input_line.setDisabled(True)
+        self.port_dropdown.setDisabled(False)
+        self.baudrate_dropdown.setDisabled(False)
 
     def connect(self):
         selected_port = self.port_dropdown.currentText()
         selected_baudrate = self.baudrate_dropdown.currentText()
         logging.info(f"Connecting to {selected_port} with baudrate {selected_baudrate}")
-        #TODO Implement connection logic here
+        self.s = serialCom(selected_port, int(selected_baudrate))
+        self.connect_button.setText("Disconnect")
+        self.connect_button.clicked.disconnect()
+        self.connect_button.clicked.connect(self.disconnect)
+        self.input_line.setDisabled(False)
+        self.port_dropdown.setDisabled(True)
+        self.baudrate_dropdown.setDisabled(True)
+        try:
+            self.s = serialCom(selected_port, int(selected_baudrate))
+            error = self.s.open()
+            if error:
+                self.terminal_output.append(error)
+                return
+        except Exception as e:
+            logging.error(e)
+            return
+        self.terminal_output.append(f'Connected to {selected_port} with baudrate {selected_baudrate}')
 
     def update_ports(self):
         try:
-            self.ports = ['COM1', 'COM2', 'COM3', 'COM4']
+            ports = ['COM1', 'COM2', 'COM3', 'COM4', 'dev/ttyUSB0', 'dev/ttyUSB1']
+            logging.info("Updating port list")
+            logging.info(f'Found ports: {ports}')
             # self.ports = serial.tools.list_ports.comports()
+            self.port_dropdown.clear()
+            self.port_dropdown.addItems(ports)
         except Exception as e:
             logging.error(e)
 
